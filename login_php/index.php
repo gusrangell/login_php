67e628c95165f7a5b93e2a365eba88fed37f76d9
Gustavo Rangel <gustavo.rangel086@gmail.com>
2025-08-31 23:17:42 -0300

    Add files via upload
    HTML básico feito

---
+++ login_php/index.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>página de login</title>
+</head>
+<body>
+
+<h1>acesse sua conta</h1>
+<form action="" method="POST" >
+<p>
+    <label>E-mail</label>
+    <input type="text" name="email">
+</p>
+
+<p>
+    <label>senha</label>
+    <input type="password" name="senha">
+</p>
+
+<p>
+<button type="submit">entrar</button>
+</p>
+</form>
+
+
+</body>
+</html>
+
